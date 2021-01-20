8e98f769b8c530b4f3cf706efbbcb07990ac4206
João <joao.gontijo@outlook.com>
2021-01-20 09:13:35 -0300

    finish feature v2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import api from './api';
 import capa from './images/capa.jpg';
 import polaroid from './images/polaroid.png';
 import oldpaper from './images/oldpaper.jpg';
+import deceased from './images/deceased.png';
 import PerfectScrollbar from 'react-perfect-scrollbar'
 
 class App extends Component {
@@ -13,7 +14,7 @@ class App extends Component {
     selected: {},
     selectedEp: []
   }
-
+  // função para o recebimento das informações da API e Estado
   async componentDidMount() {
     const responseChar = await api.get('characters?category=Breaking+Bad');
     const responseEp = await api.get('episodes?category=Breaking+Bad');
@@ -24,6 +25,11 @@ class App extends Component {
       selected: selectedConst
     })
     this.setEpisodes(selectedConst);
+    if (selectedConst.status === 'Deceased') {
+      document.querySelector('#died').classList.remove('hidden');
+    } else {
+      document.querySelector('#died').classList.add('hidden');
+    }
   }
 
   
@@ -31,16 +37,20 @@ class App extends Component {
   render() {
     return(
       <div>
+        {/* Dropdown */}
         <div className="custom-dropdown">
           <select onChange={this.selectCharacter}>{this.state.charData.map((character, i) => 
             <option key={i} value={i}>{character.name}</option>
           )}
           </select>
-        </div> 
+        </div>
+        {/* Card */}
         <div className="card">
+          {/* Capa do card */}
           <div className="imgBox">
             <img className="imgCapa" src={capa}/>
           </div>
+          {/* Conteúdo do card */}
           <div className="detalhes">
             <img className="imgOldpaper" src={oldpaper}/>
             <div className='row1'>
@@ -95,11 +105,13 @@ class App extends Component {
                </ul>
               </PerfectScrollbar>
             </div>
+            <img id='died' className="diedLogo hidden" src={deceased}/>
           </div>
         </div>
       </div>
     )
   }
+  // Função de seleção de personagem
   selectCharacter = (data) => {
     const i = data.target.value;
     const selectedConst = { name: this.state.charData[i].name, nickname: this.state.charData[i].nickname, birthday: this.state.charData[i].birthday, status: this.state.charData[i].status, url: this.state.charData[i].img };
@@ -108,7 +120,7 @@ class App extends Component {
     });
     this.setEpisodes(selectedConst);
   }
-
+  // Função de seleção de episódios
   setEpisodes = (selectedChar) => {
     const epArray = [];
     this.state.epData.forEach((ep) => {
@@ -118,7 +130,15 @@ class App extends Component {
         }
       })
     })
+    if(epArray.length === 0) {
+      epArray.push(`Unknown`);
+    }
     this.setState({selectedEp: epArray});
+    if (selectedChar.status === 'Deceased') {
+      document.querySelector('#died').classList.remove('hidden');
+    } else {
+      document.querySelector('#died').classList.add('hidden');
+    }
   }
 }
 
